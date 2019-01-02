47b26ee686b34b52bc3f34856783e0f68bb24f35
chriztso <christophertso2017@u.northwestern.edu>
2019-01-02 11:57:44 -0800

    "Will implement Stringify.JSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+
+if(typeof obj === 'string'){
+  return '"' + obj + '"';
+}
+if(Array.isArray(obj)){
+  var result = [];
+  for(var i = 0; i < obj.length; i++){
+    result.push(stringifyJSON(obj[i])); 
+  } 
+  return '[' + result.join() + ']';
+}
+
+if(typeof obj === 'object' && obj !== null ){
+ var result = [];
+ for(var prop in obj){
+  if(prop !== 'functions' && prop !== 'undefined')
+  result.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+ }
+return '{' + result.join() + '}';  
+}
+
+
+
+
+
+return '' + obj;
+
   // your code goes here
+
 };
